e4ecbc6b7461fbd0e9f55b77c162153d7d235b3e
Yashvardhan <vs21yash@gmail.com>
2025-05-01 10:00:50 +0530

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import path from 'path';
-import tailwind from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwind()],
+  plugins: [react(), tailwindcss()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
